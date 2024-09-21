08e79ae78ef24f68fb111e81f453a47ff045ef2b
Prathamesh3061 <144711895+Prathamesh3061@users.noreply.github.com>
2024-09-21 11:38:57 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,61 @@
+const data = [
+    {
+      id: 1,
+      name: "Eiffel Tower",
+      info: "The Eiffel Tower, located in the heart of Paris, France, is one of the most recognizable structures in the world. Constructed between 1887 and 1889, it was initially designed as the centerpiece for the 1889 World's Fair held in Paris to celebrate the centennial of the French Revolution. Gustave Eiffel, a renowned engineer, was the mastermind behind this iconic iron structure.",
+      image: "https://media.istockphoto.com/id/924891460/photo/eiffel-tower-in-paris-france.jpg?s=2048x2048&w=is&k=20&c=5H1OGahMEPvUFFVn0Z9WR7bPxMZqWwGGV6JGHEb6RW0=",
+      price: "20,000"
+    },
+    {
+      id: 2,
+      name: "Great Wall of China",
+      info: "The Great Wall of China is one of the most impressive feats of engineering and construction in human history. Built over several dynasties, the wall stretches more than 13,000 miles across northern China, from Shanhaiguan in the east to Jiayuguan in the west. Originally conceived by Emperor Qin Shi Huang in the 3rd century BC as a means of defending China from invasions by nomadic tribes.",
+      image: "https://media.istockphoto.com/id/1165262128/photo/the-great-wall-of-jinshan-mountains-in-the-cloud-sea.jpg?s=2048x2048&w=is&k=20&c=OPiouDuwPHa63dV3rPfOMEl0e58uINYjAjnkubk9IVU=",
+      price: "150,000"
+    },
+    {
+      id: 3,
+      name: "Grand Canyon",
+      info: "The Grand Canyon is one of the most spectacular natural wonders in the world, located in the state of Arizona in the United States. Carved by the Colorado River over millions of years, the canyon is 277 miles long, up to 18 miles wide, and reaches a depth of over a mile in some places. The Grand Canyon is renowned for its stunning and colorful landscape",
+      image: "https://media.istockphoto.com/id/1048931984/photo/horseshoe-bend-at-sunset.jpg?s=2048x2048&w=is&k=20&c=xUkBXM3V9EWBGxUHojJ-myRhxe-v99bksUAdMOb0JXM=",
+      price: "100,000"
+    },
+    {
+      id: 4,
+      name: "Taj Mahal",
+      info: "The Taj Mahal, located in Agra, India, is widely regarded as one of the most beautiful buildings in the world. Commissioned in 1632 by the Mughal emperor Shah Jahan to house the tomb of his beloved wife Mumtaz Mahal, the Taj Mahal is an enduring symbol of love and one of the finest examples of Mughal architecture. The white marble mausoleum is set against the backdrop of the Yamuna River and is surrounded by lush gardens.",
+      image: "https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=2048x2048&w=is&k=20&c=fW1YzvoN9JHAWTTLzGSj3T2WBfhZGZPztijYeRthQrA=",
+      price: "35,000"
+    },
+    {
+      id: 5,
+      name: "Machu Picchu",
+      info: "Machu Picchu is a breathtaking archaeological site located in the Andes Mountains of Peru. Built in the 15th century by the Inca Empire, it is often referred to as the 'Lost City of the Incas' because it remained hidden from the outside world until its rediscovery in 1911 by American historian Hiram Bingham. Machu Picchu sits at an altitude of 2,430 meters (7,970 feet) above sea level",
+      image: "https://media.istockphoto.com/id/1010828398/photo/machu-picchu-inca-ruins-peru.jpg?s=2048x2048&w=is&k=20&c=2Bp7-Cgji_X0M9bIcyWG4jwiiJN7xxGJKPMgjURl3F4=",
+      price: "250,000"
+    },
+    {
+      id: 6,
+      name: "Colosseum",
+      info: "The Colosseum, located in the heart of Rome, Italy, is an iconic symbol of the Roman Empire and one of the greatest architectural feats of ancient times. Built between AD 70 and AD 80 during the reign of Emperor Vespasian, the Colosseum was used for gladiatorial contests, public spectacles, and other forms of entertainment, including animal hunts, executions, and mock sea battles.",
+      image: "https://media.istockphoto.com/id/1290101405/photo/colosseum-in-rome-with-morning-sun.jpg?s=2048x2048&w=is&k=20&c=Ne-mM6qSYMh9_5joLbPnwySglqZpt9wwFfOlN23lRbY=",
+      price: "200,000"
+    },
+    {
+      id: 7,
+      name: "Statue of Liberty",
+      info: "The Statue of Liberty, standing on Liberty Island in New York Harbor, is one of the most enduring symbols of freedom and democracy in the world. Designed by French sculptor Frédéric Auguste Bartholdi and gifted to the United States by France in 1886 to celebrate the centennial of American independence, the statue represents Libertas, the Roman goddess of freedom.",
+      image: "https://media.istockphoto.com/id/1167975236/photo/the-statue-of-liberty-over-the-scene-of-new-york-cityscape-river-side-which-location-is-lower.jpg?s=2048x2048&w=is&k=20&c=kI3yFcg6JYU0DklNJG3I46gx1lg_rFWJ388dvaHokXs=",
+      price: "90,000"
+    },
+    {
+      id: 8,
+      name: "Santorini",
+      info: "Santorini, a picturesque island in the Aegean Sea, is one of the most popular travel destinations in Greece. Known for its stunning white-washed buildings, blue-domed churches, and breathtaking sunsets, Santorini offers visitors a unique blend of natural beauty and rich history. The island was formed by a massive volcanic eruption around 3,600 years ago, which created its famous crescent-shaped caldera.",
+      image: "https://media.istockphoto.com/id/1397763152/photo/oia-town-on-santorini-island-greece-traditional-and-famous-houses-and-churches-with-blue.jpg?s=2048x2048&w=is&k=20&c=vqOINP7v-5w3_a_q9POxGOAu5RW3ZoPmsZk3QNcx010=",
+      price: "220,000"
+    }
+  ];
+
+  export default data;
+  
